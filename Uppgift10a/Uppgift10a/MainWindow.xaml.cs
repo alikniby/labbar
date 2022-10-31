c9e1323da0a45fb94f7c7a729b919f670a133c7d
Ali kniby <kniby@gmail.com>
2022-10-31 09:13:36 +0100

    Uppgift 10a

---
+++ Uppgift10a/Uppgift10a/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift10a
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TxtAge_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+        }
+
+        private void BtnCheck_Click(object sender, RoutedEventArgs e)
+
+        {
+            int age;
+            string name;
+            Convert.ToString(TxtName.Text);
+            name = TxtName.Text;
+            Convert.ToInt32(TxtAge.Text);
+            age = Convert.ToInt32(TxtAge.Text);
+            if (age < 15)
+            {
+                TxtRes.Text = "Hej " + name +" Du är " + age + " du får kolla på filmer med åldersgränsen 11 år ";
+            }
+            if (age == 15)
+            {
+                TxtRes.Text = "Hej " + name + " Du är " + age + " alla i sällskapet måste vara 15 år eller äldre ";
+            }
+            if (age > 15)
+            {
+                TxtRes.Text = "Hej " + name + " Du är " + age + " du får kolla på alla filmer";
+            }
+            if (age <= 8)
+            {
+                TxtRes.Text = "Hej " + name + " Du är " + age + " du får kolla på filmer med åldersgränsen 7 år ";
+            }
+            
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+
+        {
+   
+            int age;
+            string name;
+            Convert.ToString(TxtName.Text);
+            name = TxtName.Text;
+            Convert.ToInt32(TxtAge.Text);
+            age = Convert.ToInt32(TxtAge.Text);
+            if (age < 15)
+            {
+                BtnCheck_Click(sender, e);
+                TxtRes.Text ="sjukatt" + age;
+            }
+        }
+    }
+}
---
+++ Uppgift7/Uppgift7/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Btntext_Click(object sender, RoutedEventArgs e)
+        {
+
+            string vis = "dölj lösenord";
+            if (LblPassword.IsVisible)
+            {
+                LblPassword.Visibility = Visibility.Hidden;
+                Btnhidden.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                Btnhidden.Visibility = Visibility.Visible;
+                LblPassword.Visibility = Visibility.Visible;
+                Btntext.Visibility = Visibility.Hidden;
+            }
+
+
+        }
+
+        private void Btnhidden_Click(object sender, RoutedEventArgs e)
+        {
+           
+                LblPassword.Visibility = Visibility.Hidden;
+                Btnhidden.Visibility = Visibility.Hidden;
+                Btntext.Visibility = Visibility.Visible;
+            }
+
+        }
+    }
+
